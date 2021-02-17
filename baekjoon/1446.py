c1fd65552ab13c6b0071e0609ab408c1551eacbf
uoayop <uoayop@kakao.com>
2021-02-17 17:57:39 +0900

    210217

---
+++ baekjoon/1446.py
@@ -0,0 +1,54 @@
+#지름길
+#최단거리 
+#https://www.acmicpc.net/problem/1446
+
+import sys
+import collections
+input = sys.stdin.readline
+
+
+# < 거리를 시작 지점에서 1씩 이동하면서 각 지점에 대한 다익스트라 처리하기 >
+# 1씩 이동하면서  if (그 지점에 지름길이 있으면 )
+#   : 해당 지점에서 지름길을 타고 도착하는 경우 vs 현재 다익스트라 배열에 저장된 값을 비교 --> 최솟값으로 갱신함
+# else (그 지점에 지름길이 없으면)
+#   : 이전 지점 + 1 vs 현재 지점 다익스트라 배열에 저장된 값을 비교 --> 최솟값으로 갱신
+
+
+#지름길 개수 n, 고속도로 길이 d
+n, d = map(int,input().rstrip().rsplit())
+graph = [[] for _ in range(n)]
+# 다익스트라 배열과 비교해서 더 작은 값을 배열에 넣어야하므로 큰 값으로 배열을 초기화 해준다.
+moved_distance = [99999 for _ in range(d+1)]
+
+for i in range(n):
+    start, end, length = map(int,input().rstrip().rsplit())
+    graph[i]=(start,end,length)
+graph.sort()
+
+def drive():
+    now_length = 0
+    now_index = 0
+    moved_distance[0] = 0
+    while now_length < d:
+        while now_index < n:
+            start, end, length = (graph[now_index])[0], (graph[now_index])[1], (graph[now_index])[2]
+
+            if start != now_length:
+                break
+            if end <= d:
+                # 지름길로 이동
+                compare = moved_distance[now_length] + length
+                if compare < moved_distance[end]:
+                    moved_distance[end] = compare
+
+            now_index += 1
+        
+        # 한칸 이동
+        if moved_distance[now_length] + 1 < moved_distance[now_length + 1] :
+            moved_distance[now_length + 1] = moved_distance[now_length] + 1
+
+        now_length += 1
+
+    print(moved_distance[d])
+
+drive()
--- baekjoon/4485.py
+++ baekjoon/4485.py
@@ -3,3 +3,88 @@
 #https://www.acmicpc.net/problem/4485
 
 import sys
+from collections import deque
+input = sys.stdin.readline
+
+cnt = 0
+while(1):
+    #동굴 크기 n
+    n = int(input().rstrip())
+    cnt += 1
+    if n == 0 :
+        break
+    
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    cave = [[0 for _ in range(n)] for _ in range(n)] 
+    theif = [[99999 for _ in range(n)] for _ in range(n)] 
+    visited = [[False for _ in range(n)] for _ in range(n)] 
+    for i in range(n):
+        row = (input().rstrip()).replace(' ','')
+        for j,char in enumerate(row):
+            if char != " ":
+                cave[i][j]=int(char)
+
+    q = deque()
+    q.append((0,0))
+    theif[0][0] = cave[0][0] 
+
+    while q:
+        i, j = q.popleft()
+        visited[0][0]=True
+
+        for k in range(4):
+            nx = i + dx[k]
+            ny = j + dy[k]
+            if 0 <= nx < n and 0 <= ny < n:
+                if not visited[nx][ny]:
+                    if theif[nx][ny] > theif[i][j] + cave[nx][ny]:
+                        theif[nx][ny] = theif[i][j] + cave[nx][ny]
+                        q.append((nx,ny))
+
+    print("Problem {0}: {1}".format(cnt,theif[n-1][n-1]))
+
+
+# # 녹색 옷 입은 애가 젤다지?
+# # 다익스트라 알고리즘
+# # https://www.acmicpc.net/problem/4485
+
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+# #동굴 크기 n
+# n = int(input().rstrip())
+# dx = [1,-1,0,0]
+# dy = [0,0,1,-1]
+# cave = [[0 for _ in range(n)] for _ in range(n)] 
+# theif = [[9999 for _ in range(n)] for _ in range(n)] 
+# visited = [[False for _ in range(n+1)] for _ in range(n+1)]
+# for i in range(n):
+#     row = (input().rstrip()).replace(' ','')
+#     for j,char in enumerate(row):
+#         if char != " ":
+#             cave[i][j]=int(char)
+
+# q = deque()
+# q.append((0,0))
+# theif[0][0] = cave[0][0] 
+
+# while q:
+#     print(q)
+#     i, j = q.popleft()
+#     current_value = theif[i][j]
+#     #왜 . . .? 
+#     visited[0][0] = True
+
+#     for k in range(4):
+#         nx = i + dx[k]
+#         ny = j + dy[k]
+#         if not visited[nx][ny]:
+#             if 0 <= nx < n and 0 <= ny < n:
+#                 if theif[nx][ny] > current_value + cave[nx][ny]:
+#                     theif[nx][ny] = current_value + cave[nx][ny]
+#                     q.append((nx,ny))
+                    
+
+# print(theif)
+
